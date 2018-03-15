418dfdfe572a7d6a248a88e6c2218b962867f073
Tom Alston <tom.alston@dearmansystems.com>
2018-03-15 10:00:10 -0700

    This Version is basically complete...or at least I don't want to work on it any more :)

---
+++ Head_First_405_LumberJack_Que/Head_First_405_LumberJack_Que/PrimaryWebForm.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrimaryWebForm.aspx.cs" Inherits="Head_First_405_LumberJack_Que.PrimaryWebForm" %>
+
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="/MainStyleSheet.css">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+  <title></title>
+</head>
+<body>
+  <form id="form1" runat="server">
+    <div>
+      <div class="wrapper">
+        <div class="one">
+          Lumberjack Name 
+          <br />
+          <asp:TextBox ID="TextBox1" CssClass="textbox" runat="server" Width="198px"></asp:TextBox>
+          <br />
+          <br />
+          <asp:Button ID="AddLumberJackButton" CssClass="buttonModify" runat="server" Text="Add Lumberjack" Width="210px" OnClick="AddLumberJackButton_Click" />
+          <br />
+        </div>
+        <div class="two">
+          <br />
+          Breakfast Line
+          <br />
+          <asp:ListBox ID="Line" runat="server" Height="176px" Width="210px"></asp:ListBox>
+          <br />
+          <br />
+          Output Window<br />
+          <asp:ListBox ID="OutputListBox" runat="server" Width="210px" Height="150px"></asp:ListBox>
+          <br />
+        </div>
+        <div class="three">
+          <br />
+          Feed A Lumberjack (Enter # of Lumberjacks)<br />
+          <asp:TextBox ID="PanCakeNumBox1" runat="server" Height="59px" Width="210px" GroupName="FlapJacks"></asp:TextBox>
+
+          <br />
+          <br />
+          <asp:RadioButton ID="CrispyRadioButton1" runat="server" EnableViewState="False" GroupName="FlapJacks" />
+          Crispy
+          <br />
+          <asp:RadioButton ID="SoggyRadioButton1" runat="server" GroupName="FlapJacks" />
+          Soggy
+          <br />
+          <asp:RadioButton ID="BrownedRadioButton1" runat="server" GroupName="FlapJacks" />
+          Browned
+          <br />
+          <asp:RadioButton ID="BananaRadioButton2" runat="server" GroupName="FlapJacks" />
+          Banana
+          <br />
+          <br />
+          <asp:Button ID="Addflapjacks" CssClass="buttonModify" runat="server" Text="Add Flapjacks" Width="210px" OnClick="Addflapjacks_Click" />
+          <br />
+          <br />
+          <asp:Label ID="nextInLine" CssClas="labelModify" runat="server" Text=""></asp:Label>
+          <br />
+          <br />
+          <asp:Button ID="NextLumberJackButton" CssClass="buttonModify" runat="server" Text="Next Lumberjack" Width="210px" OnClick="NextLumberJackButton_Click" />
+        </div>
+      </div>
+    </div>
+  </form>
+</body>
+</html>
---
+++ Head_First_405_LumberJack_Que/Head_First_405_LumberJack_Que/PrimaryWebForm.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text; 
+
+namespace Head_First_405_LumberJack_Que
+{
+  public partial class PrimaryWebForm : System.Web.UI.Page
+  {
+    private static Queue<Lumberjack> breakfastLine = new Queue<Lumberjack>();
+
+    
+    private void RedrawList()
+    {
+      int number = 1;
+      Line.Items.Clear();
+      foreach (Lumberjack lumberJackCycle in breakfastLine)
+      {
+        Line.Items.Add(number + "." + lumberJackCycle.Name);
+        number++;
+      }
+     
+      if (breakfastLine.Count == 0)
+      {
+        CrispyRadioButton1.Enabled = false;
+        SoggyRadioButton1.Enabled = false;
+        BrownedRadioButton1.Enabled = false;
+        BananaRadioButton2.Enabled = false;
+        nextInLine.Text = ""; 
+      }
+      else
+      {
+        CrispyRadioButton1.Enabled =true;
+        SoggyRadioButton1.Enabled = true;
+        BrownedRadioButton1.Enabled = true;
+        BananaRadioButton2.Enabled = true;
+        Lumberjack currentLumberjack = breakfastLine.Peek();
+        nextInLine.Text = currentLumberjack.Name + " has "
+        + currentLumberjack.FlapjackCount + " flapjacks";
+      }
+    }
+    //The EatFlapjacks method uses a while loop to print out the lumberjack’s meal.
+
+    public enum Flapjack
+    {
+      Crispy,
+      Soggy,
+      Browned,
+      Banana,
+    }
+    class Lumberjack
+    {
+      
+      private string name;
+      public string Name
+      {
+        get { return name; }
+        set { name = value; }
+      }
+
+      private Stack<Flapjack> meal;
+
+      // Constructor
+      public Lumberjack(string name)
+      {
+        this.name = name;
+        meal = new Stack<Flapjack>();
+      }
+
+
+      public int FlapjackCount
+      {
+        get { return meal.Count; }
+        set { }
+      }
+
+      public void TakeFlapjacks(Flapjack cookedHow, int howMany)
+      {
+        for (int i = 0; i < howMany; i++)
+        {
+          meal.Push(cookedHow);
+        }
+      }
+
+      public string EatFlapjacks()
+      {
+        return (name + "’s eating flapjacks");
+      }
+
+      public List<string> EatFlapjacks2()
+      {
+        List<String> listOfFlapJackStrings = new List<string>(); 
+        foreach (Flapjack flapInstance in meal)
+        {
+          listOfFlapJackStrings.Add(name + " ate a "+ flapInstance.ToString().ToLower() + " flapjack, ");
+        }
+         return listOfFlapJackStrings; 
+          
+      }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    
+    }
+
+    protected void NextLumberJackButton_Click(object sender, EventArgs e)
+    {
+      if (breakfastLine.Count == 0) return;
+      Lumberjack nextLumberjack = breakfastLine.Dequeue();
+      OutputListBox.Items.Add(nextLumberjack.EatFlapjacks());
+      foreach(string strings in nextLumberjack.EatFlapjacks2())
+      {
+      OutputListBox.Items.Add(strings);
+      }
+
+      nextInLine.Text = "";
+      RedrawList();
+    }
+
+    protected void AddLumberJackButton_Click(object sender, EventArgs e)
+    {
+      if (String.IsNullOrEmpty(TextBox1.Text)) return;
+      breakfastLine.Enqueue(new Lumberjack(TextBox1.Text));
+      TextBox1.Text = "";
+      RedrawList(); 
+      if(breakfastLine.Count <= 0)
+      {
+        // do somthing
+      }
+    }
+
+    protected void Addflapjacks_Click(object sender, EventArgs e)
+    {
+
+      if (PanCakeNumBox1.Text == "") return; 
+
+      if (breakfastLine.Count == 0) return;
+      Flapjack food;
+      if (CrispyRadioButton1.Checked == true)
+      {
+        food = Flapjack.Crispy;
+      }
+      else if (SoggyRadioButton1.Checked == true)
+      {
+        food = Flapjack.Soggy;
+      }
+      else if (BrownedRadioButton1.Checked == true)
+      {
+        food = Flapjack.Soggy;
+      }
+      else
+      {
+        food = Flapjack.Banana;
+      }
+
+      Lumberjack currentlumberjack = breakfastLine.Peek();
+      int numOfPankakes = Int32.Parse(PanCakeNumBox1.Text);
+      currentlumberjack.TakeFlapjacks(food, numOfPankakes);
+      RedrawList(); 
+    }
+  }
+}
